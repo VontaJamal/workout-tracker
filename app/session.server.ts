ef997a1335fd9104f1ba31a463aadf71d428a278
Vonta Jamal <djwstech@gmail.com>
2026-09-23 16:36:34 -0400

    Allow HTTP session cookie only in local smoke server

--- app/session.server.ts
+++ app/session.server.ts
@@ -13,7 +13,9 @@ export const sessionStorage = createCookieSessionStorage({
     path: "/",
     sameSite: "lax",
     secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === "production",
+    secure:
+      process.env.NODE_ENV === "production" &&
+      process.env.LOCAL_HTTP_SESSION_COOKIE !== "true",
   },
 });
 
